4458664ab79f4b58f9bf5fac1b49bd087d9fb970
Kye <kye@apacmediasolutions.com>
2023-07-13 15:37:55 -0400

    clean up

--- testing/benchmarking.py
+++ testing/benchmarking.py
@@ -60,3 +60,53 @@ ax.set_ylabel('Time (seconds)')
 plt.show()
 
 print(f"Throughput: {throughput} instances/second")
+
+
+import tracemalloc
+
+# Memory Footprint
+tracemalloc.start()
+model.forward(torch.randint(0, 64006, (1, 8192)))
+current, peak = tracemalloc.get_traced_memory()
+tracemalloc.stop()
+
+# Sequence Length Impact
+seq_lengths = [1024, 2048, 4096, 8192]
+seq_impact_times = []
+for length in seq_lengths:
+    start_time = time.time()
+    model.forward(torch.randint(0, 64006, (1, length)))
+    end_time = time.time()
+    seq_impact_times.append(end_time - start_time)
+
+# Consistency Over Time
+consistency_times = []
+for _ in range(10):
+    start_time = time.time()
+    model.forward(torch.randint(0, 64006, (1, 8192)))
+    end_time = time.time()
+    consistency_times.append(end_time - start_time)
+
+# Graphical Interface
+fig, axs = plt.subplots(3)
+
+axs[0].bar(["Forward Pass Time", "Backward Pass Time", "End-to-End Latency"], [forward_pass_time, backward_pass_time, end_to_end_latency])
+axs[0].set_title('Speed Metrics')
+axs[0].set_xlabel('Metrics')
+axs[0].set_ylabel('Time (seconds)')
+
+axs[1].bar(seq_lengths, seq_impact_times)
+axs[1].set_title('Sequence Length Impact')
+axs[1].set_xlabel('Sequence Length')
+axs[1].set_ylabel('Time (seconds)')
+
+axs[2].plot(list(range(1, 11)), consistency_times)
+axs[2].set_title('Consistency Over Time')
+axs[2].set_xlabel('Run Number')
+axs[2].set_ylabel('Time (seconds)')
+
+plt.tight_layout()
+plt.show()
+
+print(f"Throughput: {throughput} instances/second")
+print(f"Memory used: {current / 10**6}MB; Peak: {peak / 10**6}MB")
