62849c4c9563b5b747df73fd227fc042f950d191
Kye <kye@apacmediasolutions.com>
2023-07-19 22:33:48 -0400

    clean up

--- Andromeda/model.py
+++ Andromeda/model.py
@@ -1,7 +1,7 @@
 import torch 
 from torch.nn import Module
 import bitsandbytes
-from Andromeda.optimus_prime import TransformerWrapper, AutoregressiveWrapper, AndromedaEmbedding, Decoder
+from optimus_prime import TransformerWrapper, AutoregressiveWrapper, AndromedaEmbedding, Decoder
 from transformers import AutoTokenizer
 
 class AndromedaTokenizer:
