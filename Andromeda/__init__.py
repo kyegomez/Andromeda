81e1470be35400012d6c61f0e26cee9508ae44f0
Kye <kye@apacmediasolutions.com>
2023-07-19 22:30:28 -0400

    clean up

--- Andromeda/__init__.py
+++ Andromeda/__init__.py
@@ -1,3 +1,3 @@
-from Andromeda.model import AndromedaTokenizer, Andromeda
-from Andromeda.train_distributed import Train
-from Andromeda.build_dataset import DatasetBuilder
+from model import AndromedaTokenizer, Andromeda
+from train_distributed import Train
+from build_dataset import DatasetBuilder
