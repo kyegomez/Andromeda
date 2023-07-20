f17d4013a3350e73b291d6db9f4fa07c12528cdf
Kye <kye@apacmediasolutions.com>
2023-07-19 22:59:01 -0400

    clean up

--- Andromeda/train_distributed.py
+++ Andromeda/train_distributed.py
@@ -645,20 +645,20 @@ def Train():
             )
 
 
-def main():
-    # os.environ['MASTER_ADDR'] = 'localhost'
-    # os.environ['MASTER_PORT'] = '9994'
+# def main():
+#     # os.environ['MASTER_ADDR'] = 'localhost'
+#     # os.environ['MASTER_PORT'] = '9994'
     
-    # # [CRITICAL] Pay attention to this when scaling to multiple GPUs and clusters
+#     # # [CRITICAL] Pay attention to this when scaling to multiple GPUs and clusters
     
-    # # Pay attention to this, use "accelerate config"
+#     # # Pay attention to this, use "accelerate config"
 
-    # os.environ['RANK']       = str(0) # Number of nodes (servers)
-    # os.environ['WORLD_SIZE'] = str(torch.cuda.device_count())
+#     # os.environ['RANK']       = str(0) # Number of nodes (servers)
+#     # os.environ['WORLD_SIZE'] = str(torch.cuda.device_count())
 
-    torch.distributed.init_process_group()
+#     torch.distributed.init_process_group()
     
-    Train()
+#     Train()
 
 if __name__ == '__main__':
     main()
