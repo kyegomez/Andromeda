ac43a4f3a11103ad77ce392acd63a574ea063878
Kye <kye@apacmediasolutions.com>
2023-07-24 12:31:18 -0400

    v1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'TheBestLLMEver',
   packages = find_packages(exclude=['examples']),
-  version = '0.0.3',
+  version = '0.0.1',
   license='MIT',
   description = 'andromeda - Pytorch',
   author = 'Kye Gomez',
