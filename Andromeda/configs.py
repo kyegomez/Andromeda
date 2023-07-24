73237c3db362ca09f2f5b2074d120c27af9a5e95
Kye <kye@apacmediasolutions.com>
2023-07-24 13:53:35 -0400

    configs for each andromeda size model

---
+++ Andromeda/configs.py
@@ -0,0 +1,125 @@
+from andromeda.model import Andromeda
+
+Andromeda1Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=2048,
+    depth=16,
+    dim_head=128,
+    heads=8,
+    use_abs_pos_emb=False, 
+    alibi_pos_bias=True, 
+    alibi_num_heads=4, 
+    rotary_xpos=True,
+    attn_flash=True, 
+    shift_tokens=1, 
+    attn_one_kv_head=True, 
+    qk_norm=True, 
+    attn_qk_norm=True, 
+    attn_qk_norm_dim_scale=True, 
+    embedding_provider=AndromedaEmbedding()
+)
+
+
+
+Andromeda3Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=3072,
+    depth=24,
+    dim_head=128,
+    heads=12,
+    use_abs_pos_emb=False, 
+    alibi_pos_bias=True, 
+    alibi_num_heads=6, 
+    rotary_xpos=True,
+    attn_flash=True, 
+    shift_tokens=1, 
+    attn_one_kv_head=True, 
+    qk_norm=True, 
+    attn_qk_norm=True, 
+    attn_qk_norm_dim_scale=True, 
+    embedding_provider=AndromedaEmbedding()
+)
+
+
+
+Andromeda7Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=4096,
+    depth=32,
+    dim_head=128,
+    heads=16,
+    use_abs_pos_emb=False, 
+    alibi_pos_bias=True, 
+    alibi_num_heads=8, 
+    rotary_xpos=True,
+    attn_flash=True, 
+    shift_tokens=1, 
+    attn_one_kv_head=True, 
+    qk_norm=True, 
+    attn_qk_norm=True, 
+    attn_qk_norm_dim_scale=True, 
+    embedding_provider=AndromedaEmbedding()
+)
+
+Andromeda10Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=5120,
+    depth=32,
+    dim_head=128,
+    heads=20,
+    use_abs_pos_emb=False, 
+    alibi_pos_bias=True, 
+    alibi_num_heads=4, 
+    rotary_xpos=True,
+    attn_flash=True, 
+    shift_tokens=1, 
+    attn_one_kv_head=True, 
+    qk_norm=True, 
+    attn_qk_norm=True, 
+    attn_qk_norm_dim_scale=True, 
+    embedding_provider=AndromedaEmbedding()
+)
+
+Andromeda15Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=6144,
+    depth=40,
+    dim_head=128,
+    heads=24,
+    use_abs_pos_emb=False, 
+    alibi_pos_bias=True, 
+    alibi_num_heads=4, 
+    rotary_xpos=True,
+    attn_flash=True, 
+    shift_tokens=1, 
+    attn_one_kv_head=True, 
+    qk_norm=True, 
+    attn_qk_norm=True, 
+    attn_qk_norm_dim_scale=True, 
+    embedding_provider=AndromedaEmbedding()
+)
+
+Andromeda20Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=7168,
+    depth=48,
+    dim_head=128,
+    heads=28,
+    use_abs_pos_emb=False, 
+    alibi_pos_bias=True, 
+    alibi_num_heads=4, 
+    rotary_xpos=True,
+    attn_flash=True, 
+    shift_tokens=1, 
+    attn_one_kv_head=True, 
+    qk_norm=True, 
+    attn_qk_norm=True, 
+    attn_qk_norm_dim_scale=True, 
+    embedding_provider=AndromedaEmbedding()
+)
