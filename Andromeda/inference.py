4e1a1ed0f79d48a27604672fcaada893e8b7b563
Kye <kye@apacmediasolutions.com>
2023-07-26 09:11:27 -0400

    inference cleanup

--- Andromeda/inference.py
+++ Andromeda/inference.py
@@ -5,124 +5,124 @@ from einops._torch_specific import allow_ops_in_compiled_graph
 import argparse
 from Andromeda.model import AndromedaTokenizer, Andromeda
 
-class AndromedaEval:
-    def __init__(self, path, seed=42, device=None):
-        self.path = path
-        self.seed = seed
+# class AndromedaEval:
+#     def __init__(self, path, seed=42, device=None):
+#         self.path = path
+#         self.seed = seed
 
-        self.device = device
+#         self.device = device
 
-        if self.device is None:
-            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#         if self.device is None:
+#             self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        set_seed(self.seed)
+#         set_seed(self.seed)
 
-        #tokenizer
-        self.tokenizer = AndromedaTokenizer
+#         #tokenizer
+#         self.tokenizer = AndromedaTokenizer
 
-        #model
-        self.model = Andromeda
+#         #model
+#         self.model = Andromeda
 
-        #checkpoint
-        self.model.load_state_dict(torch.load(self.path))
-        self.model.eval()
+#         #checkpoint
+#         self.model.load_state_dict(torch.load(self.path))
+#         self.model.eval()
 
-        #device
-        self.model = self.model.to(self.device)
+#         #device
+#         self.model = self.model.to(self.device)
 
-        #metrics
-        self.metrics = {}
-        self.reset_metrics()
+#         #metrics
+#         self.metrics = {}
+#         self.reset_metrics()
 
-    def reset_metrics(self):
-        self.metrics = {
-            "generation_steps": None,
-            "time_forward": [],
-            "time_forward_average": None,
+#     def reset_metrics(self):
+#         self.metrics = {
+#             "generation_steps": None,
+#             "time_forward": [],
+#             "time_forward_average": None,
 
-            "memory_usages": [],
-            "memory_usage_average": None,
-            "time_end_to_end": None,
+#             "memory_usages": [],
+#             "memory_usage_average": None,
+#             "time_end_to_end": None,
             
-            "throughput": None
-        }
+#             "throughput": None
+#         }
 
-    def get_num_params(self):
-        num_params = sum(param.numel() for param in self.model.parameters() if param.requires_grad)
+#     def get_num_params(self):
+#         num_params = sum(param.numel() for param in self.model.parameters() if param.requires_grad)
 
-        return num_params
+#         return num_params
     
-    def generate(self, prompt, generation_steps=32):
-        #make sure all of the metrics reset at every generation
-        self.reset_metrics()
+#     def generate(self, prompt, generation_steps=32):
+#         #make sure all of the metrics reset at every generation
+#         self.reset_metrics()
 
-        self.metrics["generation_steps"] = generation_steps
+#         self.metrics["generation_steps"] = generation_steps
 
-        tokens = self.tokenizer.encode(prompt)
-        tokens_new = []
+#         tokens = self.tokenizer.encode(prompt)
+#         tokens_new = []
         
-        time_end_to_end = time.time()
+#         time_end_to_end = time.time()
 
-        #generation loop
-        for _ in range(generation_steps):
-            tokens_tensor = torch.tensor([tokens], device=self.device)
+#         #generation loop
+#         for _ in range(generation_steps):
+#             tokens_tensor = torch.tensor([tokens], device=self.device)
 
-            #forward pass
-            tracemalloc.start()
+#             #forward pass
+#             tracemalloc.start()
 
-            time_forward_0 = time.time()
+#             time_forward_0 = time.time()
             
-            logits = self.model(tokens_tensor, return_loss=False)[:, -1] # no loss takes the output of the last tokens
+#             logits = self.model(tokens_tensor, return_loss=False)[:, -1] # no loss takes the output of the last tokens
             
-            time_forward_1 = time.time()
+#             time_forward_1 = time.time()
             
-            _, memory_usage = tracemalloc.get_traced_memory()
-            tracemalloc.stop()
+#             _, memory_usage = tracemalloc.get_traced_memory()
+#             tracemalloc.stop()
 
-            self.metrics["memory_usages"].append(memory_usage)
+#             self.metrics["memory_usages"].append(memory_usage)
 
-            time_forward = time_forward_1 - time_forward_0
-            self.metrics["times_forward"].append(time_forward)
+#             time_forward = time_forward_1 - time_forward_0
+#             self.metrics["times_forward"].append(time_forward)
 
-            next_token = torch.armax(logits).item()
+#             next_token = torch.armax(logits).item()
 
-            #save the newly generated token
-            tokens.append(next_token)
-            tokens_new.append(next_token)
+#             #save the newly generated token
+#             tokens.append(next_token)
+#             tokens_new.append(next_token)
 
-        time_end_to_end_1 = time.time()
+#         time_end_to_end_1 = time.time()
 
-        time_end_to_end = time_end_to_end_1 - time_end_to_end_0
-        self.metrics["time_end_to_end"] = time_end_to_end
+#         time_end_to_end = time_end_to_end_1 - time_end_to_end_0
+#         self.metrics["time_end_to_end"] = time_end_to_end
 
-        decoded = self.tokenizer.decode(tokens)
+#         decoded = self.tokenizer.decode(tokens)
 
-        self.metrics["time_forward_average"] = np.mean(self.metrics["times_forward"])
-        self.metrics["memory_usage_average"] = np.mean(self.metrics["memory_usage"])
+#         self.metrics["time_forward_average"] = np.mean(self.metrics["times_forward"])
+#         self.metrics["memory_usage_average"] = np.mean(self.metrics["memory_usage"])
 
-        self.metrics['throughput'] = generation_steps / np.sum(self.metrics["times_forward"])
+#         self.metrics['throughput'] = generation_steps / np.sum(self.metrics["times_forward"])
 
-        return tokens_new, decoded
+#         return tokens_new, decoded
 
         
 
 
-def main():
-    prompt = 'My name is'
+# def main():
+#     prompt = 'My name is'
 
-    andromeda = EvalAndromeda(path='checkpoints/step_44927_6656/pytorch_model.bin')
+#     andromeda = EvalAndromeda(path='checkpoints/step_44927_6656/pytorch_model.bin')
     
-    num_params = Andromeda.get_num_params()
-    print(f'The model has {num_params} parameters')
+#     num_params = Andromeda.get_num_params()
+#     print(f'The model has {num_params} parameters')
     
-    _, output = Andromeda.generate(prompt)
+#     _, output = Andromeda.generate(prompt)
     
-    for metric, value in Andromeda.metrics.items():
-        print(f'{metric}: {value}\n')
+#     for metric, value in Andromeda.metrics.items():
+#         print(f'{metric}: {value}\n')
     
-    print('\n')
+#     print('\n')
     
-    print(output)
+#     print(output)
 
 
 
